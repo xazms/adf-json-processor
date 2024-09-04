de166377ffa9e6f4f985d95491280ef62f92f866
xazms <xazms@energinet.dk>
2024-09-04 14:42:59 +0200

    Changed back

--- src/adf_json_processor/auth/auth_strategy.py
+++ src/adf_json_processor/auth/auth_strategy.py
@@ -89,6 +89,7 @@ def authenticate(auth_method="PAT", dbutils=None):
         if not pat:
             raise ValueError("Personal Access Token (PAT) is required but not provided.")
         auth_strategy = PATAuthStrategy(pat)
+        # Uncomment if you need to log or print: print("Using PAT Authentication Strategy.")
 
     elif auth_method == "OAuth2":
         # Retrieve the OAuth2 token from the Databricks widget
@@ -96,6 +97,7 @@ def authenticate(auth_method="PAT", dbutils=None):
         if not token:
             raise ValueError("OAuth2 Token is required but not provided.")
         auth_strategy = OAuth2AuthStrategy(token)
+        # Uncomment if you need to log or print: print("Using OAuth2 Authentication Strategy.")
 
     else:
         raise ValueError(f"Unknown authentication method: {auth_method}. Supported methods are 'PAT' and 'OAuth2'.")
